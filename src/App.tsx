b0aada015e62b9c3a42ac9dd5cb4ef6658c87d58
Nikk7 <n.sliyusar@gmail.com>
2023-01-29 00:52:27 +0200

    start project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <span >Its should be something alco app here</span>
+    </div>
+  );
+}
+
+export default App;
